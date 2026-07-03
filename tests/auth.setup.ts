fa5e6353f1e345e3653dcced236e4ae56342851d
cabrera-rodrigo <cabrerarodrigo015@gmail.com>
2026-07-03 16:37:02 -0300

    probando

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,7 +6,7 @@ const authFile = 'playwright/.auth/user.json'
 
 setup('authenticate', async ({page}) =>{
     const loginPage = new LoginPage(page)
-    await page.goto(process.env.BASE_URL!)
+    await page.goto('https://practicesoftwaretesting.com/')
     await loginPage.loginWithCredentials(process.env.USER_EMAIL!, process.env.USER_PASSWORD!)
     await page.waitForURL('https://practicesoftwaretesting.com/account')
     await page.context().storageState({ path: authFile })
