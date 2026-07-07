8cd1468ea20ab7d7f268ba560c2c75f7ee865efe
cabrera-rodrigo <cabrerarodrigo015@gmail.com>
2026-07-07 01:06:35 -0300

    ahora si

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot: 'only-on-failure',
     trace: 'on-first-retry',
     baseURL: process.env.BASE_URL
   },
