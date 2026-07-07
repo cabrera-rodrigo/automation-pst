45bdd3d40d5edec6a2fe909756e562b260ead733
cabrera-rodrigo <cabrerarodrigo015@gmail.com>
2026-07-07 00:34:45 -0300

    probando con forzar reload

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -6,6 +6,7 @@ test('deberia loguearse correctamente', async ({page, loginPage, homePage}) => {
     await homePage.openLogin()
     await loginPage.loginWithCredentials(process.env.USER_EMAIL!, process.env.USER_PASSWORD!)
     //await page.waitForURL('https://practicesoftwaretesting.com/account')
+    await page.reload({ waitUntil: 'domcontentloaded' });
     await expect(loginPage.checkSuccessfulLogin()).toBeVisible({timeout: 15000})
     //await expect(page.getByRole('link', {name:'Sign in'})).toBeHidden()
     await page.pause()
